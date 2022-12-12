97622c122d37d75c666029a6a3cc042cd77f6bd0
Paulo Medina <paulomedinabr01@gmail.com>
2022-12-12 14:56:04 -0300

    Introdução a funções

--- outros_topicos/funcoesSintaxe.java
+++ outros_topicos/funcoesSintaxe.java
@@ -18,22 +18,34 @@ Fazer um programa para ler três números inteiros e mostrar na tela o maior del
 */
 
 public class funcoesSintaxe {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // função padrão (entry point)
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite três números: ");
+        System.out.println("Digite três números:");
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
+        int maior = max(a, b, c);  // função responsavel por encontrar o maior entre os int
+                                   
+        mostrarResultado(maior);
 
-        if (a > b && a > c) {
-            System.out.println("Maior = " + a);
-        } else if (b > c) {
-            System.out.println("Maior = " + b);
-        } else {
-            System.out.println("Maior = " + c);
-        }
         sc.close();
+    }
+
+    public static int max(int x, int y, int z) { // função personalizada
+        int aux;                                 // variavel local da função
+        if (x > y && x > z) {                    // logica para encontrar o maior int
+            aux = x;
+        } else if (y > z) {
+            aux = y;
+        } else {
+            aux = z;
+        }
+        return aux;
+    }
+
+    public static void mostrarResultado(int valor) {
+        System.out.println("maior = " + valor);
     }
 }
