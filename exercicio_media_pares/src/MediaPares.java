c39839b200cd6b004bb67a55342b8cd70b723a6f
Paulo Medina <paulomedinabr01@gmail.com>
2022-12-25 12:11:26 -0300

    Exercício vetores media pares

--- exercicio_media_pares/src/MediaPares.java
+++ exercicio_media_pares/src/MediaPares.java
@@ -1,6 +1,12 @@
 import java.util.Locale;
 import java.util.Scanner;
 
+/*
+ * Fazer um programa para ler um vetor de N números inteiros. Em seguida, mostrar na tela a 
+ * média aritmética somente dos números pares lidos, com uma casa decimal. Se nenhum número 
+ * par for digitado, mostrar a mensagem "NENHUM NUMERO PAR". 
+ */
+
 public class MediaPares {
 	public static void main(String[] args) throws Exception {
 		Locale.setDefault(Locale.US);
