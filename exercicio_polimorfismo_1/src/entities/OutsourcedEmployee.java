8d64ac11c871e84917e04d817d0e3da7b96ddbb5
Paulo Medina <paulomedinabr01@gmail.com>
2023-01-11 22:19:57 -0300

    Exercício resolvido polimorfismo 1

---
+++ exercicio_polimorfismo_1/src/entities/OutsourcedEmployee.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class OutsourcedEmployee extends Employee {
+    private Double additionalCharge;
+
+    public OutsourcedEmployee() {
+        super();
+    }
+
+    public OutsourcedEmployee(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    public OutsourcedEmployee(String name, Integer hours, Double valeuPerHour, Double additionalCharge) {
+        super(name, hours, valeuPerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    @Override
+    public double payment() {
+        return super.payment() + additionalCharge * 1.1;
+    }
+}
