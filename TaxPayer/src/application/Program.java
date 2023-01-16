37114d1f07abafd5970e6a8fe2c3a8993b9e2d49
Paulomedinabr01 <paulomedinabr01@gmail.com>
2023-01-16 18:27:38 -0300

    Ajustes

--- TaxPayer/src/application/Program.java
+++ TaxPayer/src/application/Program.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
-
 import entities.Company;
 import entities.Individual;
 import entities.TaxPayer;
