3110dda9f30e910d20562c2a82c25eca3fbe0ced
Paulo Medina <paulomedinabr01@gmail.com>
2023-01-19 15:43:46 -0300

    Implementado a lógica para verificar limite de saque

--- tratamento_de_excecoes_exercicio_2/src/application/Program.java
+++ tratamento_de_excecoes_exercicio_2/src/application/Program.java
@@ -19,16 +19,22 @@ public class Program {
         System.out.print("Saldo inicial: ");
         double balance = sc.nextDouble();
         System.out.print("Limite de saque: ");
-        double withdrawlimit = sc.nextDouble();
+        double withdrawLimit = sc.nextDouble();
 
-        Account acc = new Account(number, holder, balance, withdrawlimit);
+        Account acc = new Account(number, holder, balance, withdrawLimit);
 
         System.out.println();
         System.out.print("Informe a quantia para saque: ");
         double amount = sc.nextDouble();
 
-        acc.withdraw(amount);
-        System.out.printf("Saldo atualizado: %.2f%n", acc.getBalance());
+        if (amount > acc.getWithdrawlimit()) {
+            System.out.println("Erro de saque: A quantia excede o valor limite de saque. ");
+        } else if (amount > acc.getBalance()) {
+            System.out.println("Erro de saque: Saldo insuficiente. ");
+        } else {
+            acc.withdraw(amount);
+            System.out.printf("Saldo atualizado: %.2f%n", acc.getBalance());
+        }
 
         sc.close();
 
