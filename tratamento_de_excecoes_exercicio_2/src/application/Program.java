3e794fe9ccf10364aee9c690cf829c7e57dc91f3
Paulo Medina <paulomedinabr01@gmail.com>
2023-01-19 15:13:32 -0300

    Criado a classe Program, entrada e saída de dados

---
+++ tratamento_de_excecoes_exercicio_2/src/application/Program.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Account;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe os dados da conta ");
+        System.out.print("Numero da conta: ");
+        int number = sc.nextInt();
+        System.out.print("Titular da conta: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.print("Saldo inicial: ");
+        double balance = sc.nextDouble();
+        System.out.print("Limite de saque: ");
+        double withdrawlimit = sc.nextDouble();
+
+        Account acc = new Account(number, holder, balance, withdrawlimit);
+
+        System.out.println();
+        System.out.print("Informe a quantia para saque: ");
+        double amount = sc.nextDouble();
+
+        acc.withdraw(amount);
+        System.out.printf("Saldo atualizado: %.2f%n", acc.getBalance());
+
+        sc.close();
+
+    }
+}
