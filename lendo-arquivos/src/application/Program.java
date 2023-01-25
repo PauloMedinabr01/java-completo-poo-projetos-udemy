12423be02bfb777bf94cbbbf4794430318e118ac
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-01-25 15:50:44 -0300

    Bloco try-with-resources

--- lendo-arquivos/src/application/Program.java
+++ lendo-arquivos/src/application/Program.java
@@ -3,13 +3,11 @@ package application;
 import java.io.BufferedReader;
 
 /*
- * FileReader e BufferedReader
- * Classes
-• FileReader (stream de leitura de caracteres a partir de arquivos)
-• https://docs.oracle.com/javase/10/docs/api/java/io/FileReader.html
-• BufferedReader (mais rápido)
-• https://docs.oracle.com/javase/10/docs/api/java/io/BufferedReader.html
-• https://stackoverflow.com/questions/9648811/specific-difference-betweenbufferedreader-and-filereade
+ * Bloco try-with-resources
+ * • É um bloco try que declara um ou mais recursos, e garante que esses recursos serão fechados 
+ * • ao final do bloco
+ * • Disponível no Java 7 em diante
+ * • https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
  */
 
 import java.io.File;
@@ -21,13 +19,8 @@ public class Program {
     public static void main(String[] args) throws Exception {
 
         String path = "C:\\Temp\\Texto.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
 
             while (line != null) {
@@ -36,18 +29,6 @@ public class Program {
             }
         } catch (IOException e) {
             System.out.println("Erro: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
         }
     }
 
