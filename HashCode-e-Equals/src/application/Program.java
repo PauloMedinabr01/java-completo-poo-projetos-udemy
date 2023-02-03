c369a2a3b565d7eda90337f43a8646e5bd856650
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-02-03 19:54:21 -0300

    Ajustes

--- HashCode-e-Equals/src/application/Program.java
+++ HashCode-e-Equals/src/application/Program.java
@@ -11,10 +11,14 @@ public class Program {
         System.out.println();
         System.out.println();
 
+        String s1 = "Teste";
+        String s2 = "Teste";
+
         System.out.println(c1.hashCode());
         System.out.println(c2.hashCode());
         System.out.println(c1.equals(c2));
         System.out.println(c1 == c2);
+        System.out.println(s1 == s2);
 
     }
 }
