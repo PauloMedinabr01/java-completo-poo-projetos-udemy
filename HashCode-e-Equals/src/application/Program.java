fc3d974fe2098de443c3043c7b732e65afea2411
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-02-03 19:52:13 -0300

    HashCode e Equals

---
+++ HashCode-e-Equals/src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+
+import entities.Cliente;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+
+        Cliente c1 = new Cliente("Maria", "maria@gmail.com");
+        Cliente c2 = new Cliente("Alex", "alex@gmail.com");
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        System.out.println(c1.equals(c2));
+        System.out.println(c1 == c2);
+
+    }
+}
